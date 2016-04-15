65192c4b1ca6eb2161e2d70b5580f5dfda77bd70
shuboyang <shuboyang@yhgenomics.com>
2016-04-15 17:59:01 +0800

    update framework

--- linux_build.sh
+++ linux_build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 rm -rf cmakebuild
-CMaker project:maraton head:*.h src:*.cpp,*.c
+CMaker project:MaratonFramework compiler:clang++ head:*.h src:*.cpp,*.c lib:/usr/lib/libuv.a flag:-std=c++11-pthread out:lib 
 cd cmakebuild
 cmake ./
 make -j 4
+cd cmakebuild
